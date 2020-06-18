a869296a30e6f3dfb129008be71fc158e4de4021
Charles <charles.beasse@epitech.eu>
2020-06-18 10:05:05 +0000

    code complet for combined discord.js and dialogflow

--- dialog_flow/index.js
+++ dialog_flow/index.js
@@ -1,43 +1,46 @@
+'use strict';
+
 import dialogflow from "@google-cloud/dialogflow"
 import {v4 as uuid} from 'uuid';
 
-export default function dialog (message)
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+
+export default async function dialog(message, projectId = process.env.project_id)
 {
-    /**
-     * Send a query to the dialogflow agent, and return the query result.
-     * @param {string} projectId The project to be used
-     */
-    async function runSample(projectId = process.env.project_id) {
     // A unique identifier for the given session
-    const sessionId = uuid.v4();
-    
+    const sessionId = uuid();
+
     // Create a new session
     const sessionClient = new dialogflow.SessionsClient();
-    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-    
+
+    const sessionPath = sessionClient.projectAgentSessionPath(projectId,sessionId);
+
     // The text query request.
     const request = {
-        session: sessionPath,
-        queryInput: {
+    session: sessionPath,
+    queryInput: {
         text: {
-            // The query to send to the dialogflow agent
-            text: message,
-            // The language used by the client (en-US)
-            languageCode: 'en-US',
+        // The query to send to the dialogflow agent
+        text: message,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
         },
-        },
+    },
     };
-    
+
     // Send request and log result
     const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
+    // console.log('Detected intent');
     const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
+    // console.log(`  Query: ${result.queryText}`);
+    // console.log(`  Response: ${result.fulfillmentText}`);
     if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
+    // console.log(`  Intent: ${result.intent.displayName}`);
     } else {
-        console.log(`  No intent matched.`);
+    // console.log('  No intent matched.');
     }
-    }
+    return result;
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
+
 import Discord from 'discord.js';
 const client = new Discord.Client();
 import dialog from "./dialog_flow/index.js";
 import axios from 'axios';
 import {} from 'dotenv/config.js';
+const prefix = "!";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,27 +13,13 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 
-  // DISPLAY CURRENT ETHEREUM
-  if (msg.content === '!eth') {
-    axios.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot')
-    .then(response => {
-      msg.reply('Le prix de l\'ethereum est de ' + Math.round(response.data.data.amount) + ' €');
-    })
-    .catch(error => {
-      console.log(error);
+  if(msg.content.toLowerCase().startsWith(prefix))
+  {
+    dialog(msg.content.substring(1)).then(function(result){
+      msg.reply(result.fulfillmentText);
     });
   }
 
-  // DISPLAY CURRENT BITCOIN
-  if (msg.content === '!btc') {
-    axios.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot')
-    .then(response => {
-      msg.reply('Le prix du bitcoin est de ' + Math.round(response.data.data.amount) + ' €');
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
 });
 
 client.login(process.env.bot_key);
